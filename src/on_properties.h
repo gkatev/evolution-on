08ea36bb9553be2e8788ecb060242d9d2f160522
Ozan Türkyılmaz <ozan.turkyilmaz@gmail.com>
2022-03-17 15:13:45 +0300

    Update Copyright Headers

--- src/on_properties.h
+++ src/on_properties.h
@@ -1,7 +1,8 @@
 /* Evoution On plugin
  * Copyright (C) 2008-2012 Lucian Langa <cooly@gnome.eu.org>
  * Copyright (C) 2012-2013 Kostadin Atanasov <pranayama111@gmail.com>
- *
+ * Copyright (C) 2022 Ozan Türkyılmaz <ozan.turkyilmaz@gmail.com>
+ * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
--- src/tray.c
+++ src/tray.c
@@ -1,6 +1,7 @@
 /* Evoution On plugin
  *  Copyright (C) 2008-2012 Lucian Langa <cooly@gnome.eu.org>
- *
+ *  Copyright (C) 2022 Ozan Türkyılmaz <ozan.turkyilmaz@gmail.com>
+ * 
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
