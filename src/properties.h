c61744317378c9b3a3f9a85f5022e159c621d73b
gkatev <george_kate@hotmail.com>
2025-10-09 20:56:27 +0300

    Remove dead code left over for notifications
    Notification supports was removed in #6cf69d4. The upstream
    'mail-notification' plugin provides this functionality.
    
    Some code still remains, in comments -- WIP

--- src/properties.h
+++ src/properties.h
@@ -1,20 +1,12 @@
 #ifndef EVOLUTION_ON_ON_PROPERTIES_H
 #define EVOLUTION_ON_ON_PROPERTIES_H
 
-#define GCONF_KEY_NOTIF_ROOT			"/apps/evolution/eplugin/mail-notification/"
 #define GCONF_KEY_TRAY_ROOT				"/apps/evolution/eplugin/evolution-on/"
-
-#define NOTIF_SCHEMA					"org.gnome.evolution.plugin.mail-notification"
 #define TRAY_SCHEMA						"org.gnome.evolution.plugin.evolution-on"
 
 #define CONF_KEY_HIDDEN_ON_STARTUP		"hidden-on-startup"
 #define CONF_KEY_HIDE_ON_MINIMIZE		"hide-on-minimize"
 #define CONF_KEY_HIDE_ON_CLOSE			"hide-on-close"
-#define CONF_KEY_NOTIFY_ONLY_INBOX		"notify-only-inbox"
-#define CONF_KEY_ENABLED_DBUS			"notify-dbus-enabled"
-#define CONF_KEY_ENABLED_STATUS			"notify-status-enabled"
-#define CONF_KEY_ENABLED_SOUND			"notify-sound-enabled"
-#define CONF_KEY_STATUS_NOTIFICATION	"notify-status-notification"
 
 gboolean is_part_enabled(gchar *schema, const gchar *key);
 void properties_show(void);
--- src/tray.c
+++ src/tray.c
@@ -22,10 +22,12 @@
 #include "config.h"
 #endif
 
+#include <string.h>
+
 #include <gtk/gtk.h>
 #include <glib.h>
 #include <glib/gi18n.h>
-#include <string.h>
+#include <glib/gprintf.h>
 
 #include <e-util/e-util.h>
 
@@ -33,27 +35,20 @@
 #include <shell/e-shell-view.h>
 #include <shell/e-shell-window.h>
 
-#include <libemail-engine/libemail-engine.h>
-
 #include <mail/em-event.h>
 #include <mail/em-folder-tree.h>
 #include <mail/e-mail-reader.h>
 
-#ifdef DEBUG
-#include <glib/gprintf.h>
-#endif
+#include <libemail-engine/libemail-engine.h>
 
-#include "on_icon.h"
-#include "properties.h"
 #include "sn.h"
+#include "properties.h"
 
 #define ICON_READ "mail-read"
 #define ICON_UNREAD "mail-unread"
 
-// static gulong show_window_handle = 0;
-// static gboolean show_window_cb_called = FALSE;
+static EShellWindow *evo_window;
 static gboolean initialized = FALSE;
-struct OnIcon on_icon = ONICON_NEW;
 
 //helper method for toggling used on init for hidden on startup and on tray click
 static void
@@ -62,17 +57,12 @@ toggle_window(void)
 #ifdef DEBUG
 	g_printf("Evolution-on: Function call %s\n", __func__);
 #endif
-	if (gtk_widget_get_visible(GTK_WIDGET(on_icon.evo_window))) {
-		gtk_widget_hide(GTK_WIDGET(on_icon.evo_window));
+	if (gtk_widget_get_visible(GTK_WIDGET(evo_window))) {
+		gtk_widget_hide(GTK_WIDGET(evo_window));
 	} else {
-		gtk_widget_show(GTK_WIDGET(on_icon.evo_window));
+		gtk_widget_show(GTK_WIDGET(evo_window));
 		sn_set_icon(ICON_READ);
 	}
-
-	// if (on_icon.winnotify) {
-		// set_icon(&on_icon, FALSE, _(""));
-		// on_icon.winnotify = FALSE;
-	// }
 }
 
 static void
@@ -103,101 +93,26 @@ shown_window_cb(GtkWidget *widget, gpointer user_data)
 	
 	if (!show_window_cb_called) {
 		if (is_part_enabled(TRAY_SCHEMA, CONF_KEY_HIDDEN_ON_STARTUP)) {
-			// on_icon.toggle_window_func();
 			toggle_window();
 		}
 		show_window_cb_called = TRUE;
 	}
+	
+	sn_set_icon(ICON_READ);
 }
 
 /* New email notification */
 static void
-new_notify_status(EMEventTargetFolder *t, struct OnIcon *_onicon)
+new_notify_status(EMEventTargetFolder *t)
 {
 #ifdef DEBUG
 	g_printf("Evolution-on: Function call %s\n", __func__);
 #endif
-	gchar *msg;
-
-	EShell *shell = e_shell_get_default();
-	CamelStore *store;
-	gchar *folder_name;
-	EMailBackend *backend;
-	EMailSession *session;
-	EShellBackend *shell_backend;
-
-	_onicon->uri = g_strdup(t->folder_name);
-
-	ESource *source = NULL;
-		ESourceRegistry *registry;
-	const gchar *name;
-
-	const gchar *uid;
-
-	uid = camel_service_get_uid(CAMEL_SERVICE(t->store));
-	registry = e_shell_get_registry(shell);
-	source = e_source_registry_ref_source(registry,uid);
-	name = e_source_get_display_name(source);
-
-	shell_backend = e_shell_get_backend_by_name(shell, "mail");
-
-	backend = E_MAIL_BACKEND(shell_backend);
-	session = e_mail_backend_get_session(backend);
-
-	e_mail_folder_uri_parse (CAMEL_SESSION(session), t->folder_name,
-			&store, &folder_name, NULL);
-
-	if (name != NULL)
-		folder_name = g_strdup_printf("%s/%s", name, folder_name);
-	else
-		folder_name = g_strdup(folder_name);
-
-	_onicon->status_count = t->new;
-
-	/* Translators: '%d' is the count of mails received
-	 * and '%s' is the name of the folder
-	 */
-	msg = g_strdup_printf(ngettext(
-			"You have received %d new message\nin %s.",
-		"	You have received %d new messages\nin %s.",
-			_onicon->status_count), _onicon->status_count, folder_name);
-
-	g_free(folder_name);
-	if (t->msg_sender) {
-		gchar *tmp, *str;
-
-		/* Translators: "From:" is preceding a new mail
-		 * sender address, like "From: user@example.com"
-		 */
-		str = g_strdup_printf(_("From: %s"), t->msg_sender);
-		tmp = g_strconcat(msg, "\n", str, NULL);
-
-		g_free(msg);
-		g_free(str);
-		msg = tmp;
-	}
-	if (t->msg_subject) {
-		gchar *tmp, *str;
-
-		/* Translators: "Subject:" is preceding a new mail
-		 * subject, like "Subject: It happened again"
-		 */
-		str = g_strdup_printf(_("Subject: %s"), t->msg_subject);
-		tmp = g_strconcat(msg, "\n", str, NULL);
-
-		g_free(msg);
-		g_free(str);
-		msg = tmp;
-	}
-
-	sn_set_icon(ICON_UNREAD);
-	// sn_set_tooltip(msg);
 	
+	sn_set_icon(ICON_UNREAD);
+
+	// sn_set_tooltip(msg);
 	// printf("tooltip: %s\n", msg);
-
-	// _onicon->winnotify = TRUE;
-
-	g_free(msg);
 }
 
 /* Nofity based on folder changes */
@@ -212,32 +127,31 @@ org_gnome_evolution_on_folder_changed(EPlugin *ep, EMEventTargetFolder *t)
 	 * getting the folder may block...
 	 */
 	if (t->new > 0)
-		new_notify_status(t, &on_icon);
+		new_notify_status(t);
 }
 /* Mail read nofity */
 void
 org_gnome_mail_read_notify(EPlugin *ep, EMEventTargetMessage *t)
 {
-#ifdef DEBUG
+// #ifdef DEBUG
 	g_printf("Evolution-on: Function call %s\n", __func__);
-#endif
-	if (g_atomic_int_compare_and_exchange(&on_icon.status_count, 0, 0))
-		return;
+// #endif
+	// if (g_atomic_int_compare_and_exchange(&on_icon.status_count, 0, 0))
+		// return;
 
-	CamelMessageInfo *info = camel_folder_get_message_info(t->folder, t->uid);
-	if (info) {
-		guint flags = camel_message_info_get_flags(info);
-		if (!(flags & CAMEL_MESSAGE_SEEN)) {
-			if (g_atomic_int_dec_and_test(&on_icon.status_count))
-				// set_icon(&on_icon, FALSE, _(""));
-				sn_set_icon(ICON_READ);
-		}
-#if EVOLUTION_VERSION < 31192
-		camel_folder_free_message_info(t->folder, info);
-#else
-		g_clear_object(&info);
-#endif
-	}
+	// CamelMessageInfo *info = camel_folder_get_message_info(t->folder, t->uid);
+	// if (info) {
+		// guint flags = camel_message_info_get_flags(info);
+		// if (!(flags & CAMEL_MESSAGE_SEEN)) {
+			// if (g_atomic_int_dec_and_test(&on_icon.status_count))
+				// sn_set_icon(ICON_READ);
+		// }
+// #if EVOLUTION_VERSION < 31192
+		// camel_folder_free_message_info(t->folder, info);
+// #else
+		// g_clear_object(&info);
+// #endif
+	// }
 }
 /* Change window state */
 static gboolean
@@ -298,24 +212,19 @@ e_plugin_ui_init(EUIManager *ui_manager, EShellView *shell_view)
 	monitor = gdk_display_get_monitor(display, 0);
 	gdk_monitor_get_geometry(monitor, &geometry);
 
-	on_icon.evo_window = e_shell_view_get_shell_window(shell_view);
+	evo_window = e_shell_view_get_shell_window(shell_view);
 
-	g_signal_connect(G_OBJECT(on_icon.evo_window),
+	g_signal_connect(G_OBJECT(evo_window),
 		"show", G_CALLBACK(shown_window_cb), NULL);
 
-	g_signal_connect(G_OBJECT(on_icon.evo_window), "window-state-event",
+	g_signal_connect(G_OBJECT(evo_window), "window-state-event",
 			G_CALLBACK(window_state_event), NULL);
 
-	g_signal_connect(G_OBJECT(on_icon.evo_window),
+	g_signal_connect(G_OBJECT(evo_window),
 		"delete-event", G_CALLBACK(on_widget_deleted), NULL);
 
 	if(!initialized)
 		sn_init(ICON_READ, toggle_window, do_properties, do_quit);
-
-	on_icon.properties_func = do_properties;
-	on_icon.quit_func = do_quit;
-	on_icon.toggle_window_func = toggle_window;
-	// on_icon.winnotify = FALSE;
 
 	return TRUE;
 }
